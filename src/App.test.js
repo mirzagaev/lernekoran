2ccf8f075b75cc1d59123d40a74fefa266b913a2
Mirzagayev Aydin <aydin.mirzagaev@gmx.de>
2023-04-02 06:15:43 +0200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn koran h1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/lerne koran/i);
   expect(linkElement).toBeInTheDocument();
 });
